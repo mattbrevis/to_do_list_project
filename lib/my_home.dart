59333039c7d7c2a755f00a288edec5d7246db564
Matheus Brevis <mattbrevis@gmail.com>
2023-01-28 19:08:23 -0300

    Creating repository

---
+++ lib/my_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
